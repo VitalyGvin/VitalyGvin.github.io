92054721c630f82a64097563c61455cc3a1e4945
VitalyGvin <44122290+VitalyGvin@users.noreply.github.com>
2019-02-12 20:54:23 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$("#logo-x").addClass("animated fadeIn");
+	$("#logo-p").addClass("animated fadeInLeft");
+	$("#logo-f").addClass("animated fadeInRight");
+	
+	$("#point-1").addClass("point-1");
+	$("#point-2").addClass("point-2");
+	$("#point-3").addClass("point-3");
+
+});
